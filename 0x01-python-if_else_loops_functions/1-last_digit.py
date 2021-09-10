3a7aff604cb0628f21f5c8ae98199d00dad45660
George Okumu <gokumu12@gmail.com>
2021-09-10 12:32:26 +0300

    some non-sense to be followed when remembered

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-new_number = abs(number) % 10
+new_number = abs(number) % 10  """last digit of number"""
 if number < 0:
-    new_number = -new_number
+    new_number = -new_number """brings negative number"""
 print("Last digit of {} is {} and is ".format(number, new_number), end="")
 if new_number > 5:
     print("greater than 5")
---
+++ group_diss/8-concat_edges.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[0:6]
+print(str)
+
+
+
+
+
+""" 
+
+You are not allowed to use any loops or conditional statements
+Your program should be exactly 5 lines long
+You are not allowed to create new variables
+You are not allowed to use string literals 
+
+""" """write the string == object-oriented programming with Python"""
